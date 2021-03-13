928d094120866836bd114a08d900c62bb40053cf
mantinnne <animeismylife190389>
2021-03-13 13:37:40 +0300

    fix comment

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -8,16 +8,13 @@ public class DragAndDropTest {
     @Test
     void DragAndDropTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        // нахождение элементов
         SelenideElement
                 blockA = $("#column-a"),
                 blockB = $("#column-b");
-
-
-       (blockA).dragAndDropTo(blockB); // не работает
-
-
-        actions().clickAndHold(blockA).moveToElement(blockB).perform(); // не работает
-
+        //п перенос элементов
+       (blockA).dragAndDropTo(blockB);
+       //проверка переноса элементов
         $("#column-a header").shouldHave(text("B"));
         $("#column-b header").shouldHave(text("A"));
     }
